b6a40de85f11587657fc2d0ad445a901d580e2af
Alefe de Lima Moreira <delimaalefe@gmail.com>
2019-07-02 19:08:14 -0300

    increment in security valor

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($) {
     $(".scroll").click(function(event){
         event.preventDefault();
         if (window.innerWidth <= 767.98)
-            $('html,body').animate({scrollTop:$(this.hash).offset().top-115}, 600);
+            $('html,body').animate({scrollTop:$(this.hash).offset().top-105}, 600);
         else
             $('html,body').animate({scrollTop:$(this.hash).offset().top-65}, 600);
    });
