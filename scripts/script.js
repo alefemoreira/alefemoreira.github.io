b2190258db1714a3eed04f0a60e5ea6eab633541
Alefe de Lima Moreira <delimaalefe@gmail.com>
2019-07-03 14:41:53 -0300

    add footer

--- scripts/script.js
+++ scripts/script.js
@@ -6,8 +6,4 @@ jQuery(document).ready(function($) {
         else
             $('html,body').animate({scrollTop:$(this.hash).offset().top-65}, 600);
    });
-   /*$("#softwares").hover(function(){
-       /*$("#softwares").animate({filter: "grayscale('0%')"},600);
-       console.log("hello");
-   });*/
 });
